249a53e35a4ca6d8637935ab8bf50562b01af626
nakano0518 <m15sc018.prog@gmail.com>
2020-02-27 12:52:17 +0000

    20200227

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,50 +4,66 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    protected $queue;//各メソッドから参照できるよう保持
-    
-    //各テストメソッドの前に呼ばれるメソッド。publicでも可能。
-    protected function setUp():void
+
+    protected $queue;
+
+    protected function setUp(): void
     {
         $this->queue = new Queue;
     }
-    //各テストメソッドの最後に呼ばれるメソッド。publicでも可能。
-    protected function tearDown():void
-    {
-        unset($this->queue);
-        //インスタンスはメソッド実行後破棄されるので本当は不要
-        //外部リソースなどを使用するときに状態を戻すために使う
-    }
-    
+
     public function testNewQueueIsEmpty()
     {
         $this->assertEquals(0, $this->queue->getCount());
-
     }
-    
-    public function testAnItemISAddedToTheQueue()
+
+    public function testAnItemIsAddedToTheQueue()
     {
-        $this->queue->push("green");
+        $this->queue->push('green');
         
         $this->assertEquals(1, $this->queue->getCount());
-        
     }
-    
-    public function testAnItemISRemovedFromTheQueue()
+
+    public function testAnItemIsRemovedFromTheQueue()
     {
-        $this->queue->push("green");
+        $this->queue->push('green');
         $item = $this->queue->pop();
         
         $this->assertEquals(0, $this->queue->getCount());
-        $this->assertEquals("green", $item);
+        $this->assertEquals('green', $item);
     }
-    
-    //キューからアイテム削除は既にテストしたが、順序はテストしていない
+
     public function testAnItemIsRemovedFromTheFrontOfTheQueue()
     {
         $this->queue->push('first');
         $this->queue->push('second');
-        
+
         $this->assertEquals('first', $this->queue->pop());
     }
+
+    public function testMaxNumberOfItemsCanBeAdded()
+    {
+        for ($i = 0; $i < Queue::MAX_ITEMS; $i++) {
+
+            $this->queue->push($i);
+            
+        }        
+        
+        $this->assertEquals(Queue::MAX_ITEMS, $this->queue->getCount());        
+    }
+    
+    public function testExceptionThrownWhenAddingAnItemToAFullQueue()
+    {
+        for ($i = 0; $i < Queue::MAX_ITEMS; $i++) {
+
+            $this->queue->push($i);
+            
+        }     
+
+        $this->expectException(QueueException::class);
+        
+        $this->expectExceptionMessage("Queue is full");        
+        
+        $this->queue->push("wafer thin mint");           
+    }    
 }
